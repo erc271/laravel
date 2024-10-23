07d4365df9811871b1be4d8ffebca8e8428e3e24
Ayoub <ayoubsolicode07@gmail.com>
2024-10-23 09:52:21 +0100

    tuto2

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    @yield('content')
+    <footer>
+        @copy; 2024 Tous droits réservés.
+
+    </footer>
+</body>
+</html>
