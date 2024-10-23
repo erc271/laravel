893725e6fcd08f4b7deab2a0784f34b62349a268
Ayoub <ayoubsolicode07@gmail.com>
2024-10-23 11:00:04 +0100

    tuto_2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mon application Laravel</title>
 </head>
 <body>
+    <header>
+        <h1>Mon site</h1>
+        <nav>
+            </nav>
+    </header>
 
+    @yield('content')
+
+    <footer>
+        &copy; 2023 Tous droits réservés.
+    </footer>
 </body>
 </html>
